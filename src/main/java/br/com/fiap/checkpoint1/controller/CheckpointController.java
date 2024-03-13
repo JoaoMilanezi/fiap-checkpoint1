11b18a1be679058d458243a051b18cef631b4f50
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 20:33:10 -0300

    Second Commit

--- src/main/java/br/com/fiap/checkpoint1/controller/CheckpointController.java
+++ src/main/java/br/com/fiap/checkpoint1/controller/CheckpointController.java
@@ -1,5 +1,6 @@
 package br.com.fiap.checkpoint1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class CheckpointController {
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
 
 	@GetMapping("/ping")
-	public String ping() {return "Pong";}
-	
+	public String ping() {
+
+		switch (activeProfile) {
+			case "dev":
+				return "Pong em dev";
+			case "stg":
+				return "Pong em stg";
+			case "prd":
+				return "Pong em prd";
+		}
+		return null;
+	}
 }
